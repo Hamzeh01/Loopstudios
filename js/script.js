45fa8a7ccb10d0a59a68d352549935db85557fa3
Husen Hamze <summer.00.projects@gmail.com>
2023-11-03 22:05:34 +0330

    Identify the bug

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const btnMenu = document.getElementById("btn-menu");
 const menuEl = document.getElementById("menu");
 
+// TODO: Change event handler for hamburger menu
 btnMenu.addEventListener("click", () => {
   btnMenu.classList.toggle("open");
   menuEl.classList.toggle("flex");
